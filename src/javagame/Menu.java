767ddfed63b8a5b5bcac2cd5423978d552b937ba
liwang0513 <liwang0513@gmail.com>
2014-12-12 14:35:09 -0500

    add Bucky Animation

--- src/javagame/Menu.java
+++ src/javagame/Menu.java
@@ -1,27 +1,46 @@
 package javagame;
 
+import org.lwjgl.input.Mouse;
 import org.newdawn.slick.*;
 import org.newdawn.slick.state.*;
 
 public class Menu extends BasicGameState{
-	Image face;
-	int faceX = 200;
-	int faceY = 200;
+	
+	Image playNow;
+	Image exitGame;
 	
 	public Menu(int state) {
 		
 	}
 	
 	public void init(GameContainer gc, StateBasedGame sbg) throws SlickException {
-		face = new Image("res/myface.jpg");
+		playNow = new Image("res/playNow.png");
+		exitGame = new Image("res/exitGame.png");
 	}
 	
 	public void render(GameContainer gc, StateBasedGame sbg, Graphics g) throws SlickException {
-		
+		g.drawString("Welcome to Bucky Land!", 100, 50);
+		playNow.draw(100, 100);
+		exitGame.draw(100, 200);
 	}
 	
 	public void update(GameContainer gc, StateBasedGame sbg, int delta) throws SlickException {
-
+		int posX = Mouse.getX();
+		int posY = Mouse.getY();
+		
+		// play now button
+		if (posX>100 && posX<311 && posY>209 && posY<260) {
+			if (Mouse.isButtonDown(0)) {
+				sbg.enterState(1);
+			}
+		}
+		
+		// exit button
+		if (posX>100 && posX<311 && posY>109 && posY<160) {
+			if (Mouse.isButtonDown(0)) {
+				System.exit(0);
+			}
+		}
 	}
 	
 	public int getID() {
